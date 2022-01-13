0f0219d37f0331c80bc12f341ca5defdb164cfd8
hparvez <44619503+hparvez@users.noreply.github.com>
2022-01-13 18:51:01 +0100

    Updated to bypass cloudlfare

--- parliamentary_scraper.py
+++ parliamentary_scraper.py
@@ -1,17 +1,30 @@
+import json
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Create webdriver with amended config (to bypass Cloudflare)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
 
 # Dictionary to take member name and URL of their site
 members_dict = {}
+
 
 # Iterate over 33 pages [this should be updated if there's more or less pages]
 for i in range(1, 34):
     # Define base URL
     url = f"https://members.parliament.uk/members/Commons?page={i}"
-    html = urlopen(url)
+    driver.get(url)
+    html = driver.page_source
 
     # Parse URL
-    bs = BeautifulSoup(html.read(), "html.parser")
+    bs = BeautifulSoup(html, "html.parser")
 
     # Extract HTML for each individual member
     members_full = bs.find_all(class_="card card-member")
@@ -26,14 +39,15 @@ names_address_dict = {}
 
 # Loop through each name and get the address
 for name, link in members_dict.items():
-
+    
     # Parse HTML of individual page
-    html = urlopen(link)
+    driver.get(link)
+    html = driver.page_source
     bs = BeautifulSoup(html, "html.parser")
-
+    
     # Extract the address
     address = bs.find(class_="col-md-5").contents
-
+    
     # Clean up the formatting
     address_formatted = []
     for line in address:
@@ -41,8 +55,9 @@ for name, link in members_dict.items():
             address_formatted.append(line.strip())
         except:
             pass
-
+    
     # Create record in dictionary
     names_address_dict[name] = ", ".join(address_formatted)
 
-print(names_address_dict)
+with open('results.txt', 'w') as file:
+    file.write(json.dumps(names_address_dict))
