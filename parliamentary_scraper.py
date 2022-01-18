2eb0eb8499ba5544a4ede01861f1032c384ab0f2
hparvez <44619503+hparvez@users.noreply.github.com>
2022-01-18 16:01:46 +0100

    Updated to take alternative address and output CSV

--- parliamentary_scraper.py
+++ parliamentary_scraper.py
@@ -1,10 +1,10 @@
-import json
+import csv
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Create webdriver with amended config (to bypass Cloudflare)
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
@@ -15,6 +15,9 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 # Dictionary to take member name and URL of their site
 members_dict = {}
 
+# Define header
+user_agent = 'Mozilla/5.0'
+headers={'User-Agent':user_agent} 
 
 # Iterate over 33 pages [this should be updated if there's more or less pages]
 for i in range(1, 34):
@@ -34,6 +37,7 @@ for i in range(1, 34):
     for details in members_full:
         members_dict[details.find(class_="primary-info").contents[0].strip()] = link_prefix + details["href"]
 
+
 # Dictionary to take name and formatted address
 names_address_dict = {}
 
@@ -45,19 +49,27 @@ for name, link in members_dict.items():
     html = driver.page_source
     bs = BeautifulSoup(html, "html.parser")
     
-    # Extract the address
-    address = bs.find(class_="col-md-5").contents
-    
-    # Clean up the formatting
-    address_formatted = []
-    for line in address:
-        try:
-            address_formatted.append(line.strip())
-        except:
-            pass
-    
-    # Create record in dictionary
-    names_address_dict[name] = ", ".join(address_formatted)
+    # Find all matching addreses (this will be 1 for many cases)
+    addresses = bs.select(".card-contact-info .col-md-5")
+    # If there's more than 1 address, take the address that's not Westminister
+    if len(addresses) > 1:
+        # Iterate over all addresses
+        for address in addresses:
+            # Clean up the address
+            clean_address = ", ".join(list(address.stripped_strings))
+            if "House of Commons" not in clean_address:
+                # Create record in dictionary
+                names_address_dict[name] = clean_address
+    else:
+        clean_address = ", ".join(list(addresses[0].stripped_strings))
+        # Create record in dictionary
+        names_address_dict[name] = clean_address
 
-with open('results.txt', 'w') as file:
-    file.write(json.dumps(names_address_dict))
+
+# Open CSV file
+with open("results.csv", "w") as file:
+    # Create instance of DictWriter
+    w = csv.DictWriter(file, fieldnames=["Name", "Address"])
+    # Iterate over dictionary and write rows to CSV
+    for key, value in names_address_dict.items():
+        w.writerow({"Name": key, "Address": value})
